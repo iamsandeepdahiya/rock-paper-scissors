cc4040eecef161db11aa4d70e27299ceae2efb7f
iamsandeepdahiya <iamsandeepdahiya@gmail.com>
2023-12-31 18:33:03 +0530

    score comparison, finishing touch and minor fixes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//set score to zero and games to five
+let playerscore = 0;
+let computerscore = 0;
+
+//Create a function for the entire game so that we can call it five times for each round
+function game(){
 
 //get computer choice using random() function
 function getComputerChoice(){
@@ -12,16 +18,9 @@ let playerChoice = prompt("Enter your choice - rock/paper or scissor.");
 let playerSelection = playerChoice.toLowerCase();
 let computerSelection = getComputerChoice();
 
-
-
-//set score to zero and games to five
-let playerscore = 0;
-let computerscore = 0;
-
-for (let gameCount =0; gameCount<5; gameCount) {
     
-    //play the game
-    function playRound(playerSelection, computerSelection) {
+//play the round
+function playRound(playerSelection, computerSelection) {
     if (!(playerSelection === `rock` || playerSelection === `paper` || playerSelection === `scissor`)){
         alert ("Please enter one of the correct term! rock or paper or scissor.")
     }
@@ -41,8 +40,7 @@ for (let gameCount =0; gameCount<5; gameCount) {
         else {
             alert("computer wins as computer is rock and player is scissor.");
             computerscore++;
-          
-        }
+                }
     }
 
     if (computerSelection ==`paper`){
@@ -79,14 +77,41 @@ for (let gameCount =0; gameCount<5; gameCount) {
         }
     }
     }
+
+        return playerscore;
+        return computerscore;
+
+
     }
- 
-playRound(playerSelection, getComputerChoice());
-alert(computerscore);
-alert(playerscore);
-gameCount++;
-}  
 
+    playRound(playerSelection, computerSelection);
+}
 
+//call the function five times for five rounds
+game();
+game();
+game();
+game();
+game();
 
+alert("Its end of the game as total five rounds have been finished.")
+alert("Computer score is " + computerscore + " and the player score is " + playerscore + ".");
 
+//who is winner here, comparison of scores
+function winner(){
+    if (computerscore > playerscore){
+    alert ("Computer won by " + computerscore +" to "+ playerscore +".Press OK for a new game.");
+    }
+    
+    else if (computerscore === playerscore){
+        alert ("Its a tie, you both scored " + computerscore +" - "+ playerscore +".Press OK for a new game.");
+        }
+    
+    else {
+        alert ("You won by " + playerscore +" to "+ computerscore +". Press OK for a new game.");
+        }
+    }
+    winner();
+
+//reload the entire window after game finishes
+window.location.reload();
